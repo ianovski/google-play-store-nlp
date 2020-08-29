ebebf6c1fefa75ca8e8edcb1dd86fea12f4c1b63
ianovski <ianovski.a@gmail.com>
2020-08-29 16:19:11 -0400

    save all key phrases to json

--- src/comprehend.py
+++ src/comprehend.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+from pandas import read_csv
+import pandas as pd
 
 class Comprehend():
     def __init__(self,access_key_id,secret_access_key):
@@ -15,3 +17,21 @@ class Comprehend():
             LanguageCode='en'
         )
         return(response)
+    
+    def get_all_key_phrases(self, input_file, output_file):
+        key_phrases = pd.DataFrame()
+        series = read_csv(input_file,parse_dates=True,squeeze=True)
+        col_length = len(series.columns)
+        index = 1
+        print("Writing to file ............")
+        for text in series["text"]:
+            key_phrase = self.get_key_phrases(text)
+            key_phrase["ResponseMetadata"]["OrigPhrase"] = text
+            key_phrase["ResponseMetadata"]["PhraseIndex"] = index
+            if(not index%5):
+                print("Line number = {}".format(index))
+            with open(output_file, 'a') as outfile:
+                json.dump(key_phrase, outfile, indent=2)
+                if(index!=len(series)):
+                    outfile.write(',\n')
+            index += 1

4b5d5314e87504d1658cde00ba5f6ec6d94f3564
ianovski <ianovski.a@gmail.com>
2020-09-28 08:18:36 -0400

    encapsulate review object

--- src/visualize.py
+++ src/visualize.py
@@ -36,7 +36,9 @@ class Visualize():
         self.keywords = {}
         self.model_flag = False
         self.model = None
+        self.reviews_labelled = pd.DataFrame()
         self.reviews = pd.DataFrame()
+        
 
     def show_values_barplot(self,axs, space):
         def _show_on_plot(ax):
@@ -67,10 +69,20 @@ class Visualize():
         mean_date = df.resample('M').mean().dropna() 
         mean_date.plot()
         plt.show()
+    
+    def set_reviews(self, reviews):
+        self.reviews = reviews
+    
+    def get_reviews(self):
+        return self.reviews
 
-    def plot_word_count(self,filename,index):
-        reviews = read_csv(filename,parse_dates=True,squeeze=True)
-        count = reviews[index].str.len()
+    def read_reviews_csv(self, filename):
+        reviews = read_csv(filename, parse_dates=True, squeeze=True)
+        self.set_reviews(reviews)
+
+    def plot_word_count(self,review_index):
+        reviews = self.get_reviews()
+        count = reviews[review_index].str.len()
         plt.hist(count,100,alpha = 0.5)
         plt.xlim(0, 500)
         plt.title("Word count")
@@ -88,19 +100,35 @@ class Visualize():
                     text = text + phrase["Text"] + " "
         stop_words = stopwords.words('english')
         stop_words.extend(add_stop)
-        cloud = WordCloud(stopwords=stop_words).generate(text)
+        cloud = WordCloud(stopwords=stop_words,background_color='white',colormap='coolwarm').generate(text)
         plt.imshow(cloud, interpolation='bilinear')
         plt.title('Word Cloud')
         plt.axis("off")
+        plt.gca().set_facecolor('white')
         plt.show()
     
-    def get_mean_rating_for_keywords(self,filename,labeled_keywords):
-        total_score, total_reviews = 0,0
-        reviews = read_csv(filename,parse_dates=True,squeeze=True)
+    def set_reviews_labelled(self,reviews_labelled):
+        self.reviews_labelled = reviews_labelled
+
+    def get_reviews_labelled(self):
+        return self.reviews_labelled
+
+    def save_reviews_labelled(self,filename):
+        self.get_reviews_labelled().to_csv(filename,index=False)
+
+    def get_mean_rating_for_keywords(self,reviews,labeled_keywords,label):
+        total_score, total_reviews = 0, 0
+        print("[debug] labeled_keywords = {}".format(labeled_keywords))
         for idx, review in reviews.iterrows():
             if any(keyword.lower() in review['text'].lower() for keyword in labeled_keywords['keywords']):
                 total_score += review['score']
                 total_reviews += 1
+                review['category'] = label
+                self.set_reviews_labelled(self.get_reviews_labelled().append(review))
+            else:
+                review['category'] = "unlabelled"
+                self.set_reviews_labelled(self.get_reviews_labelled().append(review))
+        
         try:
             mean_score = total_score/total_reviews
         except ZeroDivisionError:
@@ -109,17 +137,30 @@ class Visualize():
             'total_reviews':total_reviews,
             'mean_score':mean_score,
         }
+       
         return(final)
 
-    def plot_mean_ratings(self,filename,labeled_keywords):
-        ''' from aws ml workshop '''
-        num_categories = len(labeled_keywords)
+    def plot_category_count_with_unlabelled(self):
+        ax = self.get_reviews_labelled()['category'].value_counts()
+        # ax = self.reviews['category'].value_counts()
+        print("[debug] category counts = {}".format(ax))
+        ax.plot(kind='bar',
+                                    figsize=(14,8),
+                                    title="Customer review category count")
+        plt.xlabel("Review category")
+        plt.ylabel("Frequency")
+        plt.show()
 
+    def plot_mean_ratings(self,filename,labeled_keywords):
+        num_categories = len(labeled_keywords)
+        reviews = self.get_reviews()
         # Store average star ratings
         average_star_ratings = {}
         for label in labeled_keywords:
-            average_star_ratings[label] = self.get_mean_rating_for_keywords(filename,labeled_keywords[label]) 
-        df = pd.DataFrame.from_dict(average_star_ratings,orient='index')
+            average_star_ratings[label] = self.get_mean_rating_for_keywords(reviews,labeled_keywords[label],label) 
+        df = pd.DataFrame.from_dict(average_star_ratings, orient='index')
+
+        self.save_reviews_labelled('labelled_reviews.csv')
 
         # Create plot
         barplot = sns.barplot(y=df.index, x='mean_score', data = df, saturation=1)
@@ -152,19 +193,21 @@ class Visualize():
         similar_words = [a_tuple[0] for a_tuple in similar_words]
         return(similar_words)
 
-    def add_keywords(self,category,keywords):
-        similar_words = self.get_similar_words(category)
-        keywords = keywords + similar_words
+    def add_keywords(self, category, keywords):
+        print("Adding keywords: {}".format(keywords))
+        # similar_words = self.get_similar_words(category)
+        # keywords = keywords + similar_words
         self.keywords[category] = {"keywords":keywords}
 
     def plot_category_count(self,filename,labeled_keywords):
         """ From AWS ml workshop"""
         num_categories = len(labeled_keywords)
 
+        print("[debug] labeled keywords = {}".format(labeled_keywords))
         # Store average star ratings
         average_star_ratings = {}
         for label in labeled_keywords:
-            average_star_ratings[label] = self.get_mean_rating_for_keywords(filename,labeled_keywords[label]) 
+            average_star_ratings[label] = self.get_mean_rating_for_keywords(self.get_reviews(),labeled_keywords[label],label) 
         df = pd.DataFrame.from_dict(average_star_ratings,orient='index')
         barplot = sns.barplot(y=df.index, x='total_reviews', data = df, saturation=1)
 
@@ -189,7 +232,7 @@ class Visualize():
         # Show the barplot
         plt.show()
     
-    def sum_ratings_per_cetegory(self,filename,labeled_keywords):
+    def sum_ratings_per_cetegory(self,labeled_keywords):
         # Create empty dictionary
         ratings_per_category = {'1star':0,
                 '2star':0,
@@ -198,7 +241,7 @@ class Visualize():
                 '5star':0}
         
         # Read csv file
-        reviews = read_csv(filename,parse_dates=True,squeeze=True)
+        reviews = self.get_reviews()
 
         # Populate dictionary with user ratings
         for idx, review in reviews.iterrows():
@@ -215,13 +258,13 @@ class Visualize():
                     ratings_per_category['5star'] +=1
         return(ratings_per_category)
 
-    def plot_rating_distribution_per_category(self,filename,labeled_keywords):
+    def plot_rating_distribution_per_category(self,labeled_keywords):
         """Partially from AWS ML workshop"""
         ratings_per_category = {}
 
         # Populate dictionary for each categories' user ratings
         for label in labeled_keywords:
-            ratings_per_category[label] = self.sum_ratings_per_cetegory(filename,labeled_keywords[label])
+            ratings_per_category[label] = self.sum_ratings_per_cetegory(labeled_keywords[label])
 
         df = pd.DataFrame.from_dict(ratings_per_category,orient='index')
         categories = ratings_per_category.keys()
@@ -314,10 +357,11 @@ class Visualize():
         plt.show()
 
     def main(self):
-        # self.plot_word_count("reviews.csv", "text")
+        self.read_reviews_csv("reviews.csv")
+        # self.plot_word_count("text")
         # stop_words = ["Rogers", "Roger", "app", "phone"]
         # self.plot_word_cloud("key_phrases.json", stop_words)
-        
+
         self.add_keywords('login', ["login", "sign", "signin","log","logging"])
         self.add_keywords('security', ["alarm", "arm", "security", "arming"])
         self.add_keywords('cameras', ["cam", "video", "record","camera","cameras"])
@@ -327,8 +371,8 @@ class Visualize():
         
         self.plot_mean_ratings("reviews.csv", self.keywords)
         self.plot_category_count_with_unlabelled()
-        # self.plot_category_count("reviews.csv", self.keywords)
-        # self.plot_rating_distribution_per_category("reviews.csv", self.keywords)
+        self.plot_category_count("reviews.csv", self.keywords)
+        self.plot_rating_distribution_per_category(self.keywords)
     
 vis = Visualize()
 vis.main()
